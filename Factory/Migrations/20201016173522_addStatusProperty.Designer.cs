7c4576eb401e1e30b49f931b0322e374bbd074d1
Ben Stoller <stollersystemsound@gmail.com>
2020-10-16 10:37:03 -0700

    update db with status properties for both models

--- Factory/Migrations/20201016173522_addStatusProperty.Designer.cs
+++ Factory/Migrations/20201016173522_addStatusProperty.Designer.cs
@@ -2,14 +2,16 @@
 using Factory.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Salon.Migrations
 {
     [DbContext(typeof(FactoryContext))]
-    partial class FactoryContextModelSnapshot : ModelSnapshot
+    [Migration("20201016173522_addStatusProperty")]
+    partial class addStatusProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Factory/Migrations/20201016173522_addStatusProperty.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Salon.Migrations
+{
+    public partial class addStatusProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MachineStatus",
+                table: "Machines",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EngineerStatus",
+                table: "Engineers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MachineStatus",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "EngineerStatus",
+                table: "Engineers");
+        }
+    }
+}
