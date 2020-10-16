f2cf11e784eacc1f837064ac2c7f8021ae42f7c1
Ben Stoller <stollersystemsound@gmail.com>
2020-10-16 12:58:51 -0700

    start building incident views

--- Factory/Migrations/20201016191203_addIncidentTable.Designer.cs
+++ Factory/Migrations/20201016191203_addIncidentTable.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Factory.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Salon.Migrations
 {
     [DbContext(typeof(FactoryContext))]
-    partial class FactoryContextModelSnapshot : ModelSnapshot
+    [Migration("20201016191203_addIncidentTable")]
+    partial class addIncidentTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
